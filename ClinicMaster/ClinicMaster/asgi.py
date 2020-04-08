cb66cc71ab60756db18e4a9998dbba03edb56e00
VirangParekh <44228173+VirangParekh@users.noreply.github.com>
2020-04-08 12:18:01 +0530

    Django Project Setup

---
+++ ClinicMaster/ClinicMaster/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ClinicMaster project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ClinicMaster.settings')
+
+application = get_asgi_application()
---
+++ ClinicMaster/DoctorWebsite/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DoctorwebsiteConfig(AppConfig):
+    name = 'DoctorWebsite'
