50cfbb3742621155ee252b701993d60958c063ec
VirangParekh <44228173+VirangParekh@users.noreply.github.com>
2020-04-11 08:50:15 +0530

    All Forms Created

---
+++ ClinicMaster/DoctorWebsite/forms.py
@@ -0,0 +1,35 @@
+from django.forms import ModelForm as mf
+from .models import Clinic, DoctorModel, Medicine, Prescription, Appointment
+
+class ClinicForm(mf):
+    class Meta:
+        model= Clinic
+        fields='__all__'
+
+class DoctorForm(mf):
+    class Meta:
+        model=DoctorModel
+        fields='__all__'
+        excluded=[
+            'user'
+        ]
+
+class Medicine(mf):  #instanciate without fail
+    class Meta:
+        model=Medicine
+        fields='__all__'
+        excluded=[
+            'patient',
+        ]
+
+class PrescriptionForm(ModelForm):
+    class Meta:
+        model=Prescription
+        fields='__all__'
+
+class Appointment(mf):
+    class Meta:
+        model= Appointment
+        fields='__all__'
+
+
