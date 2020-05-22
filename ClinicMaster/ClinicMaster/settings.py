1034275b966c2b604a2a286dff0a7e390a623a04
VirangParekh <44228173+VirangParekh@users.noreply.github.com>
2020-05-22 08:36:45 +0530

    Models Altered, Few Endpoints Created

--- ClinicMaster/ClinicMaster/settings.py
+++ ClinicMaster/ClinicMaster/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import templates
+import templates.DoctorWebsite
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'DoctorWebsite',
     'PatientApp',
 ]
---
+++ ClinicMaster/DoctorWebsite/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework.serializers import ModelSerializer as msr
+from .models import *
+from rest_framework import serializers
+
+class DoctorProfileSerializer(msr):
+
+
+    class Meta:
+        model=DoctorModel
+        fields='__all__'
+        read_only_fields=['user']
+
+
+class MedicineSerializer(msr):
+    class Meta:
+        model=Medicine
+        exclude=['patient']
+
+class PrescriptionSerializer(msr):
+    medicine=MedicineSerializer(many=False, allow_null=True)
+    class Meta:
+        model=Prescription
+        fields='__all__'
+        read_only_fields=['doctor']
